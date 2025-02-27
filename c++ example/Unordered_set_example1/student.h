3ad8ebff3c06f05b2ce628abfbc0cc548ff7fad4
Prathik <prathiksatish7135@gmail.com>
2025-02-27 09:51:00 +0530

    updated code

---
+++ c++ example/Unordered_set_example1/student.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include<iostream>
+class Student
+{
+public:
+    int m_id;
+    std::string m_name;
+    int m_Std;
+    Student(int id , std::string name , int Std);
+    Student();
+    Student& operator ==(const Student &s);
+    ~Student();
+};
+
+#endif // STUDENT_H
